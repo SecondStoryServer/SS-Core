d1c0b291d418d61101acd60fb69c1958e025c7fb
sya-ri <sya79lua@gmail.com>
2020-03-29 13:14:55 +0900

    CustomPersistentDataContainer を PersistentDataContainer の形式に合わせました

--- src/main/kotlin/me/syari/ss/core/persistentData/CustomPersistentData.kt
+++ src/main/kotlin/me/syari/ss/core/persistentData/CustomPersistentData.kt
@@ -1,5 +1,7 @@
 package me.syari.ss.core.persistentData
 
+import me.syari.ss.core.persistentData.customType.PersistentDataTypeBoolean
+import me.syari.ss.core.persistentData.customType.PersistentDataTypeUUID
 import org.bukkit.NamespacedKey
 import org.bukkit.persistence.PersistentDataContainer
 import org.bukkit.persistence.PersistentDataType
@@ -11,11 +13,11 @@ class CustomPersistentData(
 ) {
     private val String.asNamespacedKey get() = NamespacedKey(plugin, this)
 
-    private fun <T, Z> has(key: String, type: PersistentDataType<T, Z>): Boolean {
+    fun <T, Z> has(key: String, type: PersistentDataType<T, Z>): Boolean {
         return persistentDataContainer.has(key.asNamespacedKey, type)
     }
 
-    private fun <T, Z> get(key: String, type: PersistentDataType<T, Z>): Z? {
+    fun <T, Z> get(key: String, type: PersistentDataType<T, Z>): Z? {
         return if (has(key, type)) {
             persistentDataContainer.get(key.asNamespacedKey, type)
         } else {
@@ -23,57 +25,31 @@ class CustomPersistentData(
         }
     }
 
-    fun getString(key: String): String? {
-        return get(key, PersistentDataType.STRING)
+    fun <T, Z> get(key: String, type: PersistentDataType<T, Z>, default: Z): Z {
+        return get(key, type) ?: default
     }
 
-    fun getStringList(key: String): List<String>? {
-        return getString(key)?.split("§, ")
-    }
-
-    fun getBoolean(key: String, default: Boolean): Boolean {
-        return getBoolean(key) ?: default
-    }
-
-    fun getBoolean(key: String): Boolean? {
-        return get(key, PersistentDataType.BYTE) != 0.toByte()
-    }
-
-    fun getInt(key: String): Int? {
-        return get(key, PersistentDataType.INTEGER)
-    }
-
-    private fun <T, Z> set(key: String, type: PersistentDataType<T, Z>, value: Z?) {
+    fun <T, Z> set(key: String, type: PersistentDataType<T, Z>, value: Z?) {
         if (value != null) {
             persistentDataContainer.set(key.asNamespacedKey, type, value)
         } else {
-            removeKey(key)
+            remove(key)
         }
     }
 
-    fun setString(key: String, value: String?) {
-        set(key, PersistentDataType.STRING, value)
-    }
-
-    fun setStringList(key: String, value: List<String>?) {
-        set(key, PersistentDataType.STRING, value?.joinToString("§, "))
-    }
-
-    fun setBoolean(key: String, value: Boolean?) {
-        set(
-            key, PersistentDataType.BYTE, when (value) {
-                true -> 1.toByte()
-                false -> 0.toByte()
-                null -> null
-            }
-        )
-    }
-
-    fun setInt(key: String, value: Int?) {
-        set(key, PersistentDataType.INTEGER, value)
-    }
-
-    fun removeKey(key: String) {
+    fun remove(key: String) {
         persistentDataContainer.remove(key.asNamespacedKey)
+    }
+
+    companion object Type {
+        val BYTE: PersistentDataType<Byte, Byte> = PersistentDataType.BYTE
+        val SHORT: PersistentDataType<Short, Short> = PersistentDataType.SHORT
+        val INT: PersistentDataType<Int, Int> = PersistentDataType.INTEGER
+        val LONG: PersistentDataType<Long, Long> = PersistentDataType.LONG
+        val FLOAT: PersistentDataType<Float, Float> = PersistentDataType.FLOAT
+        val DOUBLE: PersistentDataType<Double, Double> = PersistentDataType.DOUBLE
+        val STRING: PersistentDataType<String, String> = PersistentDataType.STRING
+        val UUID = PersistentDataTypeUUID()
+        val BOOLEAN = PersistentDataTypeBoolean()
     }
 }
--- src/main/kotlin/me/syari/ss/core/persistentData/customType/CustomPersistentDataType.kt
+++
@@ -1,9 +0,0 @@
-package me.syari.ss.core.persistentData.customType
-
-import org.bukkit.persistence.PersistentDataType
-import java.util.*
-
-object CustomPersistentDataType {
-    val UUID: PersistentDataType<ByteArray, UUID> =
-        PersistentDataTypeUUID()
-}
