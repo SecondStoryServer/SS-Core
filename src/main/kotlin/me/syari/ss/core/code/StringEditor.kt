fdbcb5cbb92d12e4b7d884bb9579ef5ed0718681
sya-ri <sya79lua@gmail.com>
2020-03-31 21:00:06 +0900

    toColor toUncolor を Collection<String> に対応しました

--- src/main/kotlin/me/syari/ss/core/code/StringEditor.kt
+++ src/main/kotlin/me/syari/ss/core/code/StringEditor.kt
@@ -6,4 +6,8 @@ object StringEditor {
     val String.toColor get() : String = ChatColor.translateAlternateColorCodes('&', this)
 
     val String.toUncolor get() = ChatColor.stripColor(toColor) ?: this
+
+    val Collection<String>.toColor get() = map { it.toColor }
+
+    val Collection<String>.toUncolor get() = map { it.toUncolor }
 }
