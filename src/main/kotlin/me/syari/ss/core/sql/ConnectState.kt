06daf94e022e9c340a35071ab228f2bd2475522e
sya-ri <sya79lua@gmail.com>
2020-04-25 14:17:05 +0900

    ConnectState.ktを追加しました

---
+++ src/main/kotlin/me/syari/ss/core/sql/ConnectState.kt
@@ -0,0 +1,39 @@
+package me.syari.ss.core.sql
+
+/**
+ * データベース接続結果
+ * @param message 日本語メッセージ
+ */
+enum class ConnectState(val message: String) {
+    /**
+     * 成功
+     */
+    Success("成功しました"),
+
+    /**
+     * 失敗
+     */
+    CatchException("失敗しました"),
+
+    /**
+     * 設定不足
+     */
+    NullError("必要な設定が足りていません");
+
+    val isSuccess get() = this == Success
+
+    companion object {
+        /**
+         * Boolean を ConnectState に変換します
+         * @param bool canConnect(), use() の結果
+         * @return [ConnectState]
+         */
+        fun get(bool: Boolean?): ConnectState {
+            return when (bool) {
+                true -> Success
+                false -> CatchException
+                null -> NullError
+            }
+        }
+    }
+}
