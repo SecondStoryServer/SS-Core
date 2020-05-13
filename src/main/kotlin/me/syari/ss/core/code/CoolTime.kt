ef99ea281a510ef61bbd40ba5003144a79a8ad43
sya-ri <sya79lua@gmail.com>
2020-05-13 22:36:27 +0900

    クールタイムクラスの作成

---
+++ src/main/kotlin/me/syari/ss/core/code/CoolTime.kt
@@ -0,0 +1,44 @@
+package me.syari.ss.core.code
+
+import me.syari.ss.core.scheduler.CustomScheduler.runLater
+import org.bukkit.plugin.java.JavaPlugin
+
+/**
+ * 指定データ型のクールタイム
+ * @param T データ型
+ * @param plugin スケジューラに使用するプラグイン
+ */
+class CoolTime<T>(private val plugin: JavaPlugin) {
+    private val coolTimeList = mutableSetOf<T>()
+
+    /**
+     * 使用可能かどうか(クールタイム中ではないか)を取得します
+     * @param value データ
+     * @return [Boolean]
+     */
+    fun isAvailable(value: T): Boolean {
+        return !contains(value)
+    }
+
+    /**
+     * クールタイム中かどうかを取得します
+     * @param value データ
+     * @return [Boolean]
+     */
+    fun contains(value: T): Boolean {
+        return coolTimeList.contains(value)
+    }
+
+    /**
+     * クールタイム状態に変更します
+     * @param value データ
+     * @param coolTime 削除されるまでの時間 Tick
+     */
+    fun add(value: T, coolTime: Long) {
+        val success = coolTimeList.add(value)
+        if (!success) return
+        runLater(plugin, coolTime) {
+            coolTimeList.remove(value)
+        }
+    }
+}
